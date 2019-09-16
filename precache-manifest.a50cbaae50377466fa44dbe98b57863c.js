cbba15f225ffa33999065decdc1c561f3118bde7
Denys Rusov <rusovd@gmail.com>
2019-09-16 12:21:23 +0300

    Updates

---
+++ precache-manifest.a50cbaae50377466fa44dbe98b57863c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1419f2a6cad72f5850efa5a1ed709454",
+    "url": "/js-screenshot/index.html"
+  },
+  {
+    "revision": "292dee0cc7a0644972ad",
+    "url": "/js-screenshot/static/css/main.ae1064e3.chunk.css"
+  },
+  {
+    "revision": "cfd412baefc737bca309",
+    "url": "/js-screenshot/static/js/2.75970ce1.chunk.js"
+  },
+  {
+    "revision": "292dee0cc7a0644972ad",
+    "url": "/js-screenshot/static/js/main.77ce01ce.chunk.js"
+  },
+  {
+    "revision": "f573dcb61d0ec67bad69",
+    "url": "/js-screenshot/static/js/runtime~main.0d257c09.js"
+  },
+  {
+    "revision": "aa272b139c67077fd2837fb45fedbeda",
+    "url": "/js-screenshot/static/media/terrorussia.aa272b13.png"
+  }
+]);
